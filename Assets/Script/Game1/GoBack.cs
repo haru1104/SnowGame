77f127dd24027864a35506dd8b58b61bd5cc6849
HuhuMaru <85623435+HuhuMaru@users.noreply.github.com>
2021-06-27 16:30:22 +0900

    나가기 버튼
    포톤네트워크 나가기 버튼 수정

--- Assets/Script/Game1/GoBack.cs
+++ Assets/Script/Game1/GoBack.cs
@@ -2,11 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class GoBack : MonoBehaviour
+
+public class GoBack : MonoBehaviourPunCallbacks
 {
-     public void Onclick()
+    private int playerNum;
+
+    public void Onclick()
     {
-        SceneManager.LoadScene("Lobby");
+        playerNum = PhotonNetwork.PlayerList.Length;
+
+        if (photonView.IsMine == true)
+        {
+            if (photonView.Owner.IsMasterClient == true && playerNum != 1)
+            {
+                PhotonNetwork.SetMasterClient(PhotonNetwork.CurrentRoom.Players[playerNum-1]);
+               
+            }
+            PhotonNetwork.LeaveRoom();
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        PhotonNetwork.JoinLobby();
+    }
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        PhotonNetwork.JoinOrCreateRoom("Lobby",new RoomOptions { MaxPlayers = 20 },TypedLobby.Default);
+    }
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        if (PhotonNetwork.CurrentRoom.Name == "Lobby")
+        {
+            PhotonNetwork.LoadLevel("Lobby");
+        }
     }
 }
